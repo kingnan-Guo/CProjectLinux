c0ee69b54c7cb3ef0471ea31c0c280a1c8aa21dc
kingnan-Guo <373734199@qq.com>
2022-05-18 00:44:24 +0800

    创建 回声 服务器 代码

--- service/callBackService.cpp
+++ service/callBackService.cpp
@@ -14,3 +14,23 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+
+#include <string.h>
+#include <ctype.h> //C类型定义的
+#include <arpa/inet.h>//网络相关的
+
+#define SEVICE_PORT 8090
+
+int main()
+{
+    // 准备信箱
+    int sock;
+    //  创建一个信箱
+    // AF_INET   网络通信家族 TCIP 
+    // SOCK_STREAM  使用 sock协议
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    printf("sock = %u", sock);
+
+    return 0;
+}
+
