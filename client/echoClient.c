0e9dccd4ffea5ed26121b2616e0cc663a434e9ce
kingnan-Guo <373734199@qq.com>
2022-06-08 00:57:25 +0800

    更新名称

---
+++ client/echoClient.c
@@ -0,0 +1,31 @@
+/**
+ * @file echoClient.cpp
+ * @author your name (you@domain.com)
+ * @brief 客户端的 实现
+ * @version 0.1
+ * @date 2022-06-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <string.h>
+#include <ctype.h> //C类型定义的
+#include <arpa/inet.h>//网络相关的
+#include <netinet/in.h>
+
+#define SERVER_PORT 8097
+
+// argc 执行命令时参数的的个数 
+// *argv[] 执行命令时 所有的如参的 数组
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
